5e9a4b16db93b914cacb6af9d570b081940b707e
schimanskinha <129195331+schimanskinha@users.noreply.github.com>
2024-05-14 08:11:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,61 @@ botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
 }
 }
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2025-05-06T23:59");
+const tempoObjetivo2 = new Date("2024-12-31T23:59");
+const tempoObjetivo3 = new Date("2025-01-10T23:59");
+const tempoObjetivo4 = new Date("2025-01-01T23:59");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal =tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas /24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %=24;
+    if (tempoFinal > 0){
+        let contador = '';
+        contador += '<div class="contador-digito">';
+        contador += '   <p class="contador-digito-numero">'+ dias +'</p>';
+        contador += '   <p class="contador-digito-texto">dias</p>';
+        contador += '</div>';
+        contador += '<div class="contador-digito">';
+        contador += '   <p class="contador-digito-numero">'+horas+'</p>';
+        contador += '   <p class="contador-digito-texto">horas</p>';
+        contador += '</div>';
+        contador += '<div class="contador-digito">';
+        contador += '   <p class="contador-digito-numero">'+minutos+'</p>';
+        contador += '   <p class="contador-digito-texto">minutos</p>';
+        contador += '</div>';
+        contador += '<div class="contador-digito">';
+        contador += '   <p class="contador-digito-numero">'+segundos+'</p>';
+        contador += '   <p class="contador-digito-texto">segundos</p>';
+        contador += '</div>';
+
+        return contador;
+    } else {
+        return "Prazo Finalizado";
+    }
+}
+  
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].innerHTML = calculaTempo(tempos[i])
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro()
+    setInterval(atualizaCronometro, 1000)
+}
+
+comecaCronometro();
